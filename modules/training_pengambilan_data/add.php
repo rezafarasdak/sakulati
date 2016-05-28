21b9756bd92cdadd328c63673607b66b17bab7f8
Bherly Novrandy <pushm0v.development@gmail.com>
2016-05-29 02:53:02 +0700

    [Bherly] Fix pengambilan data
    - Update if data pohon is exist

--- modules/training_pengambilan_data/add.php
+++ modules/training_pengambilan_data/add.php
@@ -82,40 +82,62 @@ if($act == "add"){
 	$pbk = $_POST['pbk'];
 	$vsd = $_POST['vsd'];
 
-	$sqlInsert = "INSERT INTO objek" .
-	"(" .
-	"`id_pohon`," .
-	"`date`," .
-	"`daun_tua`," .
-	"`daun_muda`," .
-	"`bunga`," .
-	"`buah_kecil`," .
-	"`buah_dewasa`," .
-	"`buah_siap_panen`," .
-	"`PH`," .
-	"`BO`," .
-	"`KTK`," .
-	"`sehat_status`," .
-	"`pytoptora`," .
-	"`pbk`," .
-	"`vsd_status`)" .
-	"VALUES" .
-	"(" .
-	"$id_pohon_int," .
-	"NOW()," .
-	"'$id_daun_tua'," .
-	"'$id_daun_muda'," .
-	"$bunga," .
-	"$buah_kecil," .
-	"$buah_dewasa," .
-	"$buah_siap_panen," .
-	"$ph," .
-	"$bo," .
-	"$ktk," .
-	"1," .
-	"$pytoptora," .
-	"$pbk," .
-	"$vsd)";
+	$exist = $q->GetOne("select id_pohon from objek where id_pohon = ".$id_pohon_int);
+
+	if ($exist === false) {
+		$sqlInsert = "INSERT INTO objek" .
+			"(" .
+			"`id_pohon`," .
+			"`date`," .
+			"`daun_tua`," .
+			"`daun_muda`," .
+			"`bunga`," .
+			"`buah_kecil`," .
+			"`buah_dewasa`," .
+			"`buah_siap_panen`," .
+			"`PH`," .
+			"`BO`," .
+			"`KTK`," .
+			"`sehat_status`," .
+			"`pytoptora`," .
+			"`pbk`," .
+			"`vsd_status`)" .
+			"VALUES" .
+			"(" .
+			"$id_pohon_int," .
+			"NOW()," .
+			"'$id_daun_tua'," .
+			"'$id_daun_muda'," .
+			"$bunga," .
+			"$buah_kecil," .
+			"$buah_dewasa," .
+			"$buah_siap_panen," .
+			"$ph," .
+			"$bo," .
+			"$ktk," .
+			"1," .
+			"$pytoptora," .
+			"$pbk," .
+			"$vsd)";
+	}
+	else {
+		$sqlInsert = "UPDATE objek SET " .
+			"`date` = NOW()," .
+			"`daun_tua` = '$id_daun_tua'," .
+			"`daun_muda` = '$id_daun_muda'," .
+			"`bunga` = $bunga," .
+			"`buah_kecil` = $buah_kecil," .
+			"`buah_dewasa` = $buah_dewasa," .
+			"`buah_siap_panen` = $buah_siap_panen," .
+			"`PH` = $ph," .
+			"`BO` = $bo," .
+			"`KTK` = $ktk," .
+			"`sehat_status` = 1," .
+			"`pytoptora` = $pytoptora," .
+			"`pbk` = $pbk," .
+			"`vsd_status` = $vsd" .
+			" WHERE id_pohon = ".$id_pohon_int;
+	}
 
 	$rs = $q->Execute($sqlInsert);
 	if($rs){
